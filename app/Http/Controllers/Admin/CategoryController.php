85c9226e695eda7cbcfe60e06eb23696867cd504
Shayeque Ahmed <shayequeahmed16@gmail.com>
2022-09-20 22:07:23 +0530

    Index method of CategoryController implemented and show all category list on category Page

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -14,7 +14,8 @@ class CategoryController extends Controller
      * Display a listing of the resource. 
      */  
     public function index() {
-        return view('admin.Category.index');
+        $categories = Category::all();
+        return view('admin.Category.index',['categories' => $categories]);
     }
 
     /** 
--- resources/views/admin/Category/index.blade.php
+++ resources/views/admin/Category/index.blade.php
@@ -15,6 +15,7 @@
 	  <div class="col-lg-12 grid-margin stretch-card">
 	    <div class="card">
 	      <div class="card-body">
+	  			@if(count($categories) > 0)
 	        <table class="table table-striped">
 	          <thead>
 	            <tr>
@@ -27,38 +28,33 @@
 	            </tr>
 	          </thead>
 	          <tbody>
-	            <tr>
-	              <td>1</td>
-	              <td class="py-1">
-	                <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
-	              </td>
-	              <td> Herman Beck </td>
-	              <td><label class="badge badge-danger">Inactive</label></td>
-	              <td> May 15, 2015 </td>
-	              <td><i class="mdi mdi-pencil-box mdi-24px"></i></td>
-	            </tr>
-	            <tr>
-	              <td>2</td>
-	              <td class="py-1">
-	                <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
-	              </td>
-	              <td> Herman Beck </td>
-	              <td><label class="badge badge-success">Active</label></td>
-	              <td> May 15, 2015 </td>
-	              <td><i class="mdi mdi-pencil-box mdi-24px"></i></td>
-	            </tr>
-	            <tr>
-	              <td>3</td>
-	              <td class="py-1">
-	                <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
-	              </td>
-	              <td> Herman Beck </td>
-	             <td><label class="badge badge-success">Active</label></td>
-	              <td> May 15, 2015 </td>
-	              <td><i class="mdi mdi-pencil-box mdi-24px"></i></td>
-	            </tr>
+	          	@foreach($categories as $category)
+		            <tr>
+		              <td>{{ $category->id }}</td>
+		              <td class="py-1">
+		                <img src="{{asset('uploads/category')}}/{{$category->image}}" alt="image" />
+		              </td>
+		              <td> {{ $category->category }} </td>
+		              <td> 
+		              	@if($category->status == 1 )
+		              	  <label class="badge badge-success">Active</label>
+		              	@else
+		              		<label class="badge badge-danger">Inactive</label>
+		              	@endif
+		            	</td>
+		              <td> {{ date('d-m-Y', strtotime($category->created_at) ) }} </td>
+		              <td><i class="mdi mdi-pencil-box mdi-24px"></i></td>
+		            </tr>
+	            @endforeach
 	          </tbody>
 	        </table>
+	         @else
+	        <div class="card-body">
+            <blockquote class="blockquote blockquote-primary">
+              <p>No Record Found</p>
+            </blockquote>
+          </div>
+	    		 @endif
 	      </div>
 	    </div>
 	  </div>
