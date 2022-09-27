3781ce9f95bc9ed7eb44c5434546ee6ac20a5e21
Shayeque Ahmed <shayequeahmed16@gmail.com>
2022-09-27 23:07:59 +0530

    Poduct Stock table and Product Images Table inserted

---
+++ database/migrations/2022_09_27_171250_create_product_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('color_id')->constrained('colors')->onDelete('cascade');
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->index(['product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+}
